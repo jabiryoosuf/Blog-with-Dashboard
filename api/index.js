c67b3156c074b658de5c0475c3f8579cc7ca1880
jabiryoosuf <jabiribnuyoosuf@gmail.com>
2024-02-05 13:17:45 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv, { config } from "dotenv";
 
+dotenv.config()
+
+mongoose
+  .connect(process.env.MONGODB
+  )
+  .then(() => {
+    console.log("mongodb is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
-app.listen(5000, ()=>{
-    console.log("Server is running on port 5000!");
-})
+app.listen(5000, () => {
+  console.log("Server is running on port 5000!");
+});
+
+// jabiribnuyoosuf
+// c1QgjseRWC4AKeFG
